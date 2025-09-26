0fb320e20a4c36d47097cc990effeb7f56b5e3ca
Teodor0203 <toderitateodor@gmail.com>
2025-09-26 13:42:31 +0300

    feat: added button to cycle digits

--- 7segment_Display_App/7segment_Counter/7segment_Counter/main.c
+++ 7segment_Display_App/7segment_Counter/7segment_Counter/main.c
@@ -1,6 +1,21 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+uint8_t digit_index = 0;
+
+const uint8_t digits[10] = {
+	0b00111111, // 0
+	0b00000110, // 1
+	0b01011011, // 2
+	0b01001111, // 3
+	0b01100110, // 4
+	0b01101101, // 5
+	0b01111101, // 6
+	0b00000111, // 7
+	0b01111111, // 8
+	0b01101111  // 9
+};
+
 void init_devices(void)
 {
 	// interrupt pin
@@ -9,13 +24,39 @@ void init_devices(void)
 	
 	PORTD |= (1 << PD0);
 	
-	EIMSK |= (1 << INT0);
+	EIMSK |= (1 << INT0) | (1 << INT1);
 	
-	EICRA |= (1 << ISC01);
+	EICRA |= (1 << ISC01) | (1 << ISC11);
+	
+	sei();
 	
 	// GPIO pin
 	
-	DDRC |= 0xFF;
+	DDRC = 0xFF;
+	PORTC = 0x00;
+}
+
+ISR(INT0_vect)
+{
+	// cycle digits
+	
+	PORTC = digits[digit_index];
+	
+	digit_index++;
+	
+	if(digit_index > 9)
+	{
+		digit_index = 0;
+	}
+}
+
+ISR(INT1_vect)
+{
+	// reset display
+	
+	PORTC = 0x00;
+	
+	digit_index = 0;
 }
 
 
@@ -23,10 +64,9 @@ int main(void)
 {
 	init_devices();
 	
-	PORTC |= (1 << PC3) | (1 << PC2);
-    
-    while (1) 
-    {
-    }
+	while (1)
+	{
+		
+	}
 }
 
