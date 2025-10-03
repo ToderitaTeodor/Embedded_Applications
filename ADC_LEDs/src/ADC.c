534a7085647438dc5780adf3d4b441c22ca1b0bc
Teodor0203 <toderitateodor@gmail.com>
2025-10-03 19:02:11 +0300

    feat: turn on LEDs based on ADC voltage

---
+++ ADC_LEDs/src/ADC.c
@@ -0,0 +1,20 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdint.h>
+
+void ADC_init(void)
+{
+    ADMUX = (1 << REFS0); // reference to 5volts and ADC chanel 0
+    ADCSRA = (1 << ADEN) | (1 << ADPS1) | (1 << ADPS2); // enable and set prescaler to 64
+}
+
+uint16_t ADC_read(uint8_t channel)
+{
+    ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+
+    ADCSRA |= (1 << ADSC);
+
+    while(ADCSRA & (1 << ADSC));
+
+    return ADC;
+}
